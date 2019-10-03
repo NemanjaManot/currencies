432db51cfe4fa68246f85f577e26bebf8026c8df
Nemanja <manotnemanja@gmail.com>
2019-10-03 18:24:38 +0200

    added isButtonDisabled function

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -64,6 +64,8 @@ class Login extends PureComponent {
         return (!emailValidation && email.length && !passwordValidation && password.length);
     };
 
+    isButtonDisabled = () => !(this.state.password.length > 0 || this.state.email.length > 0);
+
     validationMessage = type => {
         let message;
         switch (type) {
@@ -144,6 +146,7 @@ class Login extends PureComponent {
                             contentStyle={ loginButton }
                             mode="contained"
                             onPress={ this.onSubmit }
+                            disabled={ this.isButtonDisabled() }
                             //loading={ this.state.isLoading }
                         >
                             Sign In
