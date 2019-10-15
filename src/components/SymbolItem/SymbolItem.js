0562dad700da2f574f8f083130334ff2dc772d15
Nemanja <manotnemanja@gmail.com>
2019-10-15 21:56:17 +0200

    symbol item to stateless function

--- src/components/SymbolItem/SymbolItem.js
+++ src/components/SymbolItem/SymbolItem.js
@@ -1,6 +1,5 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-import PropTypes from 'prop-types';
 import { Colors } from 'react-native-paper';
 /* Components */
 import FooterTabIcon from '../FooterTabIcon/FooterTabIcon';
@@ -12,37 +11,26 @@ import { otherColors } from '../../assets/theme';
 
 const { container, rightBoxStyle, valueStyle, labelStyle } = styles;
 
-class SymbolItem extends PureComponent {
-    static propTypes = {
-        name: PropTypes.string,
-        value: PropTypes.string,
-        isFavorite: PropTypes.bool,
-        onLabelPress: PropTypes.func,
-        onIconPress: PropTypes.func
-    };
-
-    render() {
-        const { name, isFavorite, value } = this.props;
-        return (
-            <View style={ container }>
+const SymbolItem = ({ name, isFavorite, value, onLabelPress, onIconPress }) => {
+    return (
+        <View style={ container }>
+            <TouchableOpacity
+                onPress={ onLabelPress }
+                hitSlop={ TOUCHABLE_AREA }
+            >
+                <Text style={ labelStyle }>{ name }</Text>
+            </TouchableOpacity>
+            <View style={ rightBoxStyle }>
+                <Text style={ valueStyle }>$ { value }</Text>
                 <TouchableOpacity
-                    onPress={ this.props.onLabelPress }
+                    onPress={ onIconPress }
                     hitSlop={ TOUCHABLE_AREA }
                 >
-                    <Text style={ labelStyle }>{ name }</Text>
+                    { FooterTabIcon('favorite', isFavorite ? otherColors.secondaryColor : Colors.grey400) }
                 </TouchableOpacity>
-                <View style={ rightBoxStyle }>
-                    <Text style={ valueStyle }>$ { value }</Text>
-                    <TouchableOpacity
-                        onPress={ this.props.onIconPress }
-                        hitSlop={ TOUCHABLE_AREA }
-                    >
-                        { FooterTabIcon('favorite', isFavorite ? otherColors.secondaryColor : Colors.grey400) }
-                    </TouchableOpacity>
-                </View>
             </View>
-        )
-    }
-}
+        </View>
+    )
+};
 
 export default SymbolItem;
