bb40456c600be9459cb18ceaf981305a95aae5b9
Nemanja <manotnemanja@gmail.com>
2019-10-16 22:31:25 +0200

    added flow on login screen

---
+++ type-def-libs/react-native.js
@@ -0,0 +1,15 @@
+declare module 'react-native' {
+    declare module.exports: any
+}
+
+declare module 'react-redux' {
+    declare module.exports: any
+}
+
+declare module 'react-native-paper' {
+    declare module.exports: any
+}
+
+declare module 'react-native-keyboard-aware-scroll-view' {
+    declare module.exports: any
+}
