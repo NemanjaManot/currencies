243df61826155978d544beb265b1d92ab8e5cd10
Nemanja <manotnemanja@gmail.com>
2019-10-17 23:39:32 +0200

    Added flow on reducers

--- src/screens/Login/loginReducer.js
+++ src/screens/Login/loginReducer.js
@@ -1,12 +1,19 @@
+// @flow
+
 /* Action Types */
 import { LOGIN } from './loginActionTypes';
+
+type State = {
+    errorMessage: string | null,
+    isLoginLoading: boolean
+};
 
 const INITIAL_STATE = {
     errorMessage: null,
     isLoginLoading: false
 };
 
-export default function loginReducer(state = INITIAL_STATE, action) {
+export default function loginReducer(state: State = INITIAL_STATE, action: Object): State {
     switch (action.type) {
         case LOGIN.SET_ERROR_MESSAGE: {
             return {
--- src/shared/user/userReducer.js
+++ src/shared/user/userReducer.js
@@ -1,12 +1,21 @@
+// @flow
+
+/* Action Types */
 import { USER } from './userActionTypes';
 
-const initialState = {
+type State = {
+    user: Object,
+    userId: number | null,
+    userAccount: Object
+};
+
+const INITIAL_STATE = {
     user: null,
     userId: null,
     userAccount: null
 };
 
-export default function userReducer(state = initialState, action) {
+export default function userReducer(state: State = INITIAL_STATE, action: Object): State {
     switch (action.type) {
         case USER.SET_INFO:
             return {
