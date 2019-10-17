5d7f04c27e544dfaa288cad1b5b892564bedb85f
Nemanja <manotnemanja@gmail.com>
2019-10-17 23:55:34 +0200

    Added flow for actions

--- src/screens/Market/marketActions.js
+++ src/screens/Market/marketActions.js
@@ -1,19 +1,37 @@
+// @flow
+
+/* Action types */
 import { MARKET } from './marketActionTypes';
 
-export const getSingleSymbolAction = (userId, symbolId) => ({
+type SingleSymbolType = {
+    userId: number,
+    symbolId: number
+};
+
+type WatchlistType = {
+    accountId: number,
+    symbolId: number,
+    isFollowing: boolean
+};
+
+type NewsType = {
+    offset: number
+}
+
+export const getSingleSymbolAction = (userId: number, symbolId: number): SingleSymbolType => ({
     type: MARKET.GET_SINGLE_SYMBOL,
     userId,
     symbolId
 });
 
-export const toggleWatchlistAction = (accountId, symbolId, isFollowing) => ({
+export const toggleWatchlistAction = (accountId: number, symbolId: number, isFollowing: boolean): WatchlistType => ({
     type: MARKET.TOGGLE_WATCHLIST,
     accountId,
     symbolId,
     isFollowing
 });
 
-export const getNewsAction = (offset) => ({
+export const getNewsAction = (offset: number): NewsType => ({
     type: MARKET.GET_NEWS,
     offset
 });
